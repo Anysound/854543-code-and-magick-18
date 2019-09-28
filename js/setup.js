dd04dfdacfe1b14503a4ca982c10ecca312417bb
anysound <anysoundspb@gmail.com>
2019-09-28 18:11:45 +0300

    finished

--- js/setup.js
+++ js/setup.js
@@ -1,54 +1,48 @@
+'use strict';
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 
-'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var arr = [];
+var mockData = [];
 
-var coat = document.querySelector('.wizard-coat');
-var eyes = document.querySelector('.wizard-eyes');
-var template = document.querySelector('.setup-similar-item'); //вот эти элементы
-var nameWiza = document.querySelector('.setup-similar-label'); // не находятся
-document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.overlay').classList.remove('hidden');
 
-var body = document.children[1];
-for (var i = 0; i < body.children.length; i++) {
-  var child = body.children[i];
-  console.log(child.tagName.toLocaleLowerCase());
-}
-
-function createArrWithObjs (arr) {
+function createArrWithObjs(arr) {
 
   for (var i = 0; i < 4; i++) {
     var obj = {
-        name: '',
-        coatColor: '',
-        eyesColor: ''
-      };
+      name: '',
+      coatColor: '',
+      eyesColor: ''
+    };
 
     obj.name = NAMES[i] + ' ' + SURNAMES[i];
     obj.coatColor = COAT_COLORS[i];
     obj.eyesColor = EYES_COLORS[i];
     arr.push(obj);
-    
   }
 }
-createArrWithObjs(arr);
+createArrWithObjs(mockData);
 
-// function createElements () {
-//   for (var i = 0; i < arr.length; i++) {
-//     document.querySelector('.setup-similar-label').textContent = arr[i.name];
-//     document.querySelector('.wizard-coat').style.fill = arr[i.coatColor];
-//     document.querySelector('.wizard-eyes').style.fill = arr[i.eyesColor];
-//   }
-// }
+function makeElements() {
+  var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var wrapper = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment();
 
+  for (var i = 0; i < mockData.length; i++) {
+    var clone = template.cloneNode(true);
 
+    clone.querySelector('.setup-similar-label').textContent = mockData[i].name;
+    clone.querySelector('.wizard-coat').style.fill = COAT_COLORS[i];
+    clone.querySelector('.wizard-eyes').style.fill = EYES_COLORS[i];
 
+    fragment.appendChild(clone);
+  }
+  wrapper.appendChild(fragment);
+}
 
-//document.querySelector('.setup-similar').classList.remove('hidden');
-//document.querySelector('.setup-similar-list').appendChild(template);
-//name.textContent = arr[2].name;
-console.log(coat, eyes, nameWiza, template); //coat..., eyes... null, null
+makeElements();
 
+document.querySelector('.setup-similar').classList.remove('hidden');
